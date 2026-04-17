2645159b3f9571ea1bfd90fc2e2c1b798cbb7d5e
Govind <govind12771277@gmail.com>
2026-04-17 13:57:23 +0530

    added : backend common and common package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,54 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {CreateUserSchema, SignInSchema, CreateRoomSchema} from "@repo/common/types"
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
- 
-app.get("/api/data", (req, res) => {
-  res.json({ message: "This is some data from the server!" });
+const PORT = 5000;
+
+app.post("/signup", (req, res) => {
+  // db call here
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.status(400).json({ message: "Incorrect data", errors: data.error });
+  }
+
+  res.json({ userId: "123" });
+  return;
 });
 
+app.post("/signin", (req, res) => {
+  // db call here
+
+  const data = SignInSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.status(400).json({ message: "Incorrect data", errors: data.error});
+  }
+
+  const userId = "123";
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET,
+  );
+
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", (req, res) => {
+  // db call here
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.status(400).json({ message: "Incorrect data", errors: data.error });
+  }
+
+  res.json({
+    roomId: 123,
+  });
+});
 app.listen(PORT, () => {
-    console.log("HTTP server running on port " + PORT);
-})
-
+  console.log("HTTP server running on port " + PORT);
+});
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,36 @@
 import {WebSocketServer} from 'ws' 
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const PORT = 8080;
 const wss = new WebSocketServer({port: PORT}); 
 
-console.log(`WebSocket server running on port ${PORT}`);
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws , request) => {
   console.log("Client connected");
+
+
+  const url =  request.url; 
+  if ( !url ) {
+    ws.send("No URL provided");
+    return;
+  }
+
+  const urlQueryParams = new URLSearchParams(url.split("?")[1]);
+  const token = urlQueryParams.get("token"); 
+  const decoded = jwt.verify(token as string, JWT_SECRET);
+
+  if (typeof decoded === "string") {
+    ws.send("Invalid token");
+    ws.close();
+    return;
+  }
+
+  if (!decoded || !decoded.userId) {
+    ws.send("Invalid token");
+    ws.close();
+    return;
+  }
 
   ws.on("message", (message) => {
     console.log(`Received message: ${message}`);
