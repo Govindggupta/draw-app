aae3f95884c5bb42ed7bbde329481b9761a94176
Govind <govind12771277@gmail.com>
2026-06-14 02:43:30 +0530

    added: excalidraw fontend init

---
+++ apps/excalidraw-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <h6 className="font-bold bg-yellow-300"> 
+            This is going to be the home page for the draw app website. 
+        </h6>
+    );
+}
